6b2ea4111075de0858701d3a2746a0d351e438c4
max.odendahl@t-online.de <max.odendahl@t-online.de@2d7fbac2-3451-0410-9fd9-a9db251ee21b>
2008-08-08 19:03:50 +0000

    initial batch compiling version

---
+++ trunk/officereader/bash.bat
@@ -0,0 +1,39 @@
+echo "Compiling for S60 2nd Edition"
+devices -setdefault @Series60_v20:com.nokia.series60
+
+echo "Start compiling SyExpat XML parser"
+cd SyExpat/Symbian
+bldmake bldfiles
+START ABLD.bat build armi urel
+cd ../..
+echo "Finished compiling SyExpat XML parser"
+
+echo "Start compiling Sheet Control"
+cd sheetcontrol/group
+bldmake bldfiles
+START ABLD.bat build armi urel
+cd ../..
+echo "Finished compiling Sheet Control
+
+echo "Finsihed compiling for S60 2nd Edition"
+
+
+
+echo "Compiling for S60 3nd Edition"
+devices -setdefault @S60_3rd_MR:com.nokia.s60
+
+cd SyExpat/Symbian
+bldmake bldfiles
+START ABLD.bat build gcce urel
+cd ../..
+echo "Finished compiling SyExpat XML parser"
+
+echo "Start compiling Sheet Control"
+
+cd sheetcontrol/group
+bldmake bldfiles
+START ABLD.bat build gcce urel
+cd ../..
+echo "Finished compiling Sheet Control
+
+echo "Finished for S60 3nd Edition"
